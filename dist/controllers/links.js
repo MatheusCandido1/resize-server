94c30d4e0ef9aec959d256829fa701c2f81fd737
Matheus Cândido <matheus9bh@hotmail.com>
2020-11-23 13:40:01 -0300

    add-links-functionalities-in-memo

--- dist/controllers/links.js
+++ dist/controllers/links.js
@@ -1,49 +1,45 @@
-import {Request, response, Response} from 'express';
-import {Link} from '../models/link';
-
-const links: Link[] = [];
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const links = [];
 let proxId = 1;
-
 function generateCode() {
     let text = '';
     const possibilities = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for(let i=0; i<5;i++)
+    for (let i = 0; i < 5; i++)
         text += possibilities.charAt(Math.floor(Math.random() * possibilities.length));
     return text;
 }
-
-function postLink(req: Request, res: Response) {
-    const link = req.body as Link;
+function postLink(req, res) {
+    const link = req.body;
     link.id = proxId++;
     link.code = generateCode();
     link.hits = 0;
     links.push(link);
     res.status(201).json(link);
 }
-
-function getLink(req: Request, res: Response) {
-    const code = req.params.code as string;
+function getLink(req, res) {
+    const code = req.params.code;
     const link = links.find(item => item.code === code);
-    if(!link) {
+    if (!link) {
         res.sendStatus(404);
-    } else {
+    }
+    else {
         res.json(link);
     }
 }
-
-function hitLink(req: Request, res: Response) {
-    const code = req.params.code as string;
+function hitLink(req, res) {
+    const code = req.params.code;
     const index = links.findIndex(item => item.code === code);
-    if(index === -1) {
+    if (index === -1) {
         res.sendStatus(404);
-    } else {
-        links[index].hits!++;
+    }
+    else {
+        links[index].hits++;
         res.json(links[index]);
     }
 }
-
-export default {
+exports.default = {
     postLink,
     getLink,
     hitLink
-}
+};
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,8 +5,8 @@ const router = Router();
 
 router.post('/links',  Link.postLink);
 
-router.get('/links/:code', Link.getLink);
+router.get('/links/:code', Link.hitLink);
 
-router.get('/links/:code/stats', Link.hitLink);
+router.get('/links/:code/stats', Link.getLink);
 
 export default router;
